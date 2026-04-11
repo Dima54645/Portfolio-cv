6f1b0eef0722d53a3e303a55beec3db51ea5a521
Dima54645 <titanik546@gmail.com>
2026-04-12 00:20:25 +0300

    fix: vite-config

--- src/UI/SocialIcons/SocialIcons.const.ts
+++ src/UI/SocialIcons/SocialIcons.const.ts
@@ -2,6 +2,6 @@ import { GitHub, Telegram, Email } from "@mui/icons-material";
 
 export const icons = [
   { Icon: GitHub, href: "https://github.com/Dima54645", delay: 1 },
-  { Icon: Telegram, href: "#", delay: 1.2 },
-  { Icon: Email, href: "#", delay: 1.4 },
+  { Icon: Telegram, href: "https://t.me/DmitryChichev", delay: 1.2 },
+  { Icon: Email, href: "mailto:chichev.dmitry@gmail.com", delay: 1.4 },
 ];
